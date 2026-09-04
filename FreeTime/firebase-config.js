2cd5aab437a1168561f9b82a5d6290375250fedc
qco-bd <official.qco.bd@gmail.com>
2026-09-04 08:53:24 +0600

    Update Firebase configuration and initialization

--- FreeTime/firebase-config.js
+++ FreeTime/firebase-config.js
@@ -1,12 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    updateProfile
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.firebasestorage.app",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAatpoCHMMqVm9VcFKOYdIHOa1nhiG_x1M",
+    authDomain: "freetime-794a8.firebaseapp.com",
+    projectId: "freetime-794a8",
+    storageBucket: "freetime-794a8.firebasestorage.app",
+    messagingSenderId: "972310400799",
+    appId: "1:972310400799:web:0c925fbe7ed29f6c39ea2a"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
+const auth = getAuth(app);
+
+export {
+    auth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    updateProfile
+};
